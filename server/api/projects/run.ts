df31006cbee83480dd2fdb0d7fdeb1c1bde3787b
Sauriel <sascha.muenchow@gmx.de>
2023-10-20 16:21:29 +0200

    added windows logic

--- server/api/projects/run.ts
+++ server/api/projects/run.ts
@@ -2,6 +2,5 @@ import { runScript } from "~/server/module/projectLoader";
 
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
-  console.log('run script', body);
   runScript(body.script);
 })
--- server/module/projectLoader.ts
+++ server/module/projectLoader.ts
@@ -1,22 +1,21 @@
 import path from 'path';
+import os from 'os';
 import fs from 'fs/promises';
 import { PackageJSON } from '~/types/packageJson';
 import { spawn } from 'child_process';
 
+const IS_WINDOWS = os.platform() === 'win32';
 const __dirname = process.cwd();
 
 export async function loadProject(): Promise<PackageJSON> {
-  console.log('__dirname', __dirname);
-
   const packageJsonPath = path.join(__dirname, 'package.json');
   return fs.readFile(packageJsonPath, { encoding: 'utf8' })
     .then(result => JSON.parse(result) as PackageJSON);
 }
 
 export function runScript(script: string) {
-  const command = `npm run ${script}`;
-  console.log(command);
-  const process = spawn(command, [], { cwd: __dirname });
+  const cmd = IS_WINDOWS ? 'npm.cmd' : 'npm';
+  const process = spawn(cmd, ['run', script], { cwd: __dirname });
   process.stdout.on('data', (data) => {
     console.log(`stdout: ${data}`);
   });
@@ -28,4 +27,10 @@ export function runScript(script: string) {
   process.on('close', (code) => {
     console.log(`child process exited with code ${code}`);
   });
+
+  // if (IS_WINDOWS) {
+  //   execSync(`taskkill /F /T /PID ${process.pid}`);
+  // } else {
+  //   process.kill();
+  // }
 }
